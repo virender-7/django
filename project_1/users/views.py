676b3dd303bd27171eac59a7b8ec7f68013858b5
Virender Singh Rawat <virender.7@live.com>
2026-06-06 16:13:48 +0530

    logic added for user logout

--- project_1/users/views.py
+++ project_1/users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 
 # Create your views here.
 def user_register(request): 
@@ -14,6 +14,7 @@ def user_register(request):
         form = UserCreationForm()
     return render(request, "users/user_register.html", {'form':form})
 
+
 def user_login(request):
     if request.method == "POST":
         form = AuthenticationForm(data=request.POST)
@@ -23,4 +24,11 @@ def user_login(request):
 
     else:
         form = AuthenticationForm()
-    return render(request, "users/user_login.html", {'form':form})    
+    return render(request, "users/user_login.html", {'form':form})  
+
+
+def user_logout(request):
+    if request.method== "POST":
+        logout(request)
+        return redirect('posts:list')
+
